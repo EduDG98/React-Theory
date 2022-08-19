963be6c57d54e897a56b9280264a4bd5791f2534
EduDG98 <90718854+EduDG98@users.noreply.github.com>
2022-08-19 20:11:45 +0100

    work with props

--- React-Examples/1-Example-React/react-app-0/src/App.js
+++ React-Examples/1-Example-React/react-app-0/src/App.js
@@ -2,12 +2,22 @@ import React from "react";
 import Box from './components/Box'
 import './styles/Box.css'
 
+//Create a second React component:
+const Basket = (props) => {
+    return (
+        <>
+            <h3>This is my basket of {props.fruit}</h3>
+        </>
+    )
+}
 
 //Create the main React component as an arrow function
 const App = () => {
     return (
         <>
-            <h3>Hola</h3>
+            <h3>Look how many boxes I have:</h3>
+            <Basket fruit='Oranges'></Basket>
+            <Box></Box>
             <Box></Box>
             <Box></Box>
         </>
--- React-Examples/1-Example-React/react-app-0/src/components/Box.js
+++ React-Examples/1-Example-React/react-app-0/src/components/Box.js
@@ -3,7 +3,7 @@ import React from "react";
 const Box = () => {
     return (
         <>
-            <p>Lorena</p>
+            <p>Box</p>
         </>
     )
 }
