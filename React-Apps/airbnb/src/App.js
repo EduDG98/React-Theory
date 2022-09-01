701158163b9f92edda68947e6dbf86473e5361d2
EduDG98 <90718854+EduDG98@users.noreply.github.com>
2022-09-01 19:56:37 +0100

    start airbnb app

---
+++ React-Apps/airbnb/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import logo from './images/main-logo.png';
+
+function App() {
+  return (
+    <>
+      <div className="bar-container">
+        <img src={logo} alt='Logo' />
+        <div className='center'>
+
+        </div>
+
+      </div>
+      <div className="app-container">
+        hula
+      </div>
+    </>
+  );
+}
+
+export default App;
