271bb153ae7cb9cf88e64ed8411b19c632b4b106
EduDG98 <90718854+EduDG98@users.noreply.github.com>
2022-09-02 16:28:43 +0100

    do a navbar

---
+++ React-Components/Navbar/navbar/src/Navbar.js
@@ -0,0 +1,17 @@
+import './navbar.css'
+
+export const Navbar = () => {
+    return (
+        <ul>
+            <li>
+                <a>Home</a>
+            </li>
+            <li>
+                <a>Settings</a>
+            </li>
+            <li>
+                <a>Profile</a>
+            </li>
+        </ul>
+    )
+}
