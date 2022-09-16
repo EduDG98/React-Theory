e814897434456e43cda83b6630fccda89fb1abc6
EduDG98 <90718854+EduDG98@users.noreply.github.com>
2022-09-16 18:43:45 +0100

    edit card element

--- React-Apps/airbnb/src/components/gallery-components/Card.js
+++ React-Apps/airbnb/src/components/gallery-components/Card.js
@@ -1,13 +1,22 @@
 import hotel from './../../images/hotel.png'
+import housesList from '../../data/housesList'
 
 export const Card = () => {
     return (
         <div className="card">
-            <img className='card-photo' src={hotel} />
-            <p> Place </p>
-            <p> Type</p>
-            <p> Date</p>
-            <a> Price ($)</a>
+            {
+                housesList.map((houseItem, index) => {
+                    return (
+                        <div keys={houseItem.id}>
+                            <img className='card-photo' src={houseItem.images[0]} />
+                            <p> Place </p>
+                            <p> Type</p>
+                            <p> Date</p>
+                            <a> Price ($)</a>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
---
+++ React-Apps/airbnb/src/data/housesList.js
@@ -0,0 +1,20 @@
+const housesList = [
+    {
+        "id": 1,
+        "place": "North Yorkshire",
+        "country": "UK",
+        "stars": 4.53,
+        "host": "individual",
+        "start-date": "16-21 Sept",
+        "price": 3.367,
+        "images": [
+            require("../images/houses-images/house1/pic1.webp"),
+            require("../images/houses-images/house1/pic2.webp"),
+            require("../images/houses-images/house1/pic3.webp"),
+            require("../images/houses-images/house1/pic4.webp"),
+            require("../images/houses-images/house1/pic5.webp"),
+        ]
+    }
+]
+
+export default housesList;
