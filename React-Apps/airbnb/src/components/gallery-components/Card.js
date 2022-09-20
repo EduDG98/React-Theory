09015c17a5ff5225646fe08857df08fdf94918bc
EduDG98 <90718854+EduDG98@users.noreply.github.com>
2022-09-20 20:36:18 +0100

    add card buttons

--- React-Apps/airbnb/src/components/gallery-components/Card.js
+++ React-Apps/airbnb/src/components/gallery-components/Card.js
@@ -3,12 +3,14 @@ import housesList from '../../data/housesList'
 
 export const Card = () => {
     return (
-        <div className="card">
+        <div className="card-wrapper">
             {
-                housesList.map((houseItem, index) => {
+                housesList.map((houseItem) => {
                     return (
-                        <div keys={houseItem.id}>
+                        <div keys={houseItem.id} className='card'>
                             <img className='card-photo' src={houseItem.images[0]} />
+                            <button className='card-button left-card-button'>{'<'}</button>
+                            <button className='card-button right-card-button'>{'>'}</button>
                             <p> Place </p>
                             <p> Type</p>
                             <p> Date</p>
