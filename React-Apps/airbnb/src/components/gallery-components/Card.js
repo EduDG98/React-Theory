38c0272d6e21a32559a5312817b541c6b0f74728
EduDG98 <90718854+EduDG98@users.noreply.github.com>
2022-09-21 14:00:36 +0100

    finish buttons on the card

--- React-Apps/airbnb/src/components/gallery-components/Card.js
+++ React-Apps/airbnb/src/components/gallery-components/Card.js
@@ -1,5 +1,7 @@
 import hotel from './../../images/hotel.png'
-import housesList from '../../data/housesList'
+import housesList from '../../data/housesList';
+import leftArrow from '../../images/card-buttons/left-arrow.png'
+import rightArrow from '../../images/card-buttons/right-arrow.png'
 
 export const Card = () => {
     return (
@@ -9,8 +11,8 @@ export const Card = () => {
                     return (
                         <div keys={houseItem.id} className='card'>
                             <img className='card-photo' src={houseItem.images[0]} />
-                            <button className='card-button left-card-button'>{'<'}</button>
-                            <button className='card-button right-card-button'>{'>'}</button>
+                            <button className='card-button left-card-button'><img src={leftArrow} /></button>
+                            <button className='card-button right-card-button'><img src={rightArrow} /></button>
                             <p> Place </p>
                             <p> Type</p>
                             <p> Date</p>
