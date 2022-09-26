4cbc0afbbfa681ec304cea5d474bc0c4ba9abb46
EduDG98 <90718854+EduDG98@users.noreply.github.com>
2022-09-26 13:03:28 +0100

    study useeffect

--- Hooks/useState/app-1-use-state/src/App.js
+++ Hooks/useState/app-1-use-state/src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
-import Header from './components/Header';
-import Form from './components/Form';
+import { useEffect, useState } from 'react';
 
+
+
+let count = 0;
 
 function App() {
+  const [state, setState] = useState('')
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    console.log(count)
+  }, [])
 
   return (
-    <div className="App">
-      <Header />
-      <Form />
+    <div className="app-wrapper">
+      <button onClick={() => setCount(count + 1)}>Boton</button>
+      <button onClick={() => setCount(count + 1)}>Boton</button>
+      <button onClick={() => setCount(count + 1)}>Boton</button>
+
+      <div>
+        <p>El boton pulsado es: {state}</p>
+        <p>El nº de render es: {count}</p>
+
+      </div>
     </div>
   );
 }
