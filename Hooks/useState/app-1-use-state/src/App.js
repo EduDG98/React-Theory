6e0bd6a9ff7239383799d58d13a9c8710b6382d2
EduDG98 <90718854+EduDG98@users.noreply.github.com>
2022-09-29 15:59:36 +0100

    correct useState example

--- Hooks/useState/app-1-use-state/src/App.js
+++ Hooks/useState/app-1-use-state/src/App.js
@@ -6,7 +6,6 @@ import { useEffect, useState } from 'react';
 let count = 0;
 
 function App() {
-  const [state, setState] = useState('')
   const [count, setCount] = useState(0)
 
   useEffect(() => {
@@ -15,15 +14,12 @@ function App() {
 
   return (
     <div className="app-wrapper">
-      <button onClick={() => setCount(count + 1)}>Boton</button>
-      <button onClick={() => setCount(count + 1)}>Boton</button>
-      <button onClick={() => setCount(count + 1)}>Boton</button>
-
-      <div>
-        <p>El boton pulsado es: {state}</p>
-        <p>El nº de render es: {count}</p>
-
+      <h2>The count is {count}</h2>
+      <div className='button-wrapper'>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <button onClick={() => setCount(count - 1)}>-</button>
       </div>
+      <button onClick={() => setCount(0)}>reset</button>
     </div>
   );
 }
